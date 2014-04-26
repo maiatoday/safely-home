2c0dc0c7e96b5d122f09438df4d9f21c5eb10c9b
maia grotepass <maiatoday@gmail.com>
2014-04-26 14:56:10 +0200

    Add some location code that doesn't work yet, just pasted from developer.android.com

--- app/SafelyHome/SafelyHome/src/main/java/za/co/gdgcapetown/safelyhome/MainActivity.java
+++ app/SafelyHome/SafelyHome/src/main/java/za/co/gdgcapetown/safelyhome/MainActivity.java
@@ -1,7 +1,12 @@
 package za.co.gdgcapetown.safelyhome;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -10,14 +15,32 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 
 import za.co.gdgcapetown.safelyhome.ui.IncidentFragment;
 import za.co.gdgcapetown.safelyhome.ui.PersonalDetailFragment;
 
 
-public class MainActivity extends ActionBarActivity  implements PersonalDetailFragment.OnDetailsComplete{
+public class MainActivity extends ActionBarActivity
+    implements PersonalDetailFragment.OnDetailsComplete,
+        GooglePlayServicesClient.ConnectionCallbacks,
+    GooglePlayServicesClient.OnConnectionFailedListener{
 
     private static final String TAG = MainActivity.class.toString();
+
+    /*
+     * Define a request code to send to Google Play services
+     * This code is returned in Activity.onActivityResult
+     */
+    private final static int
+        CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private ConnectionResult connectionResult;
+    private LocationClient mLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +51,19 @@ public class MainActivity extends ActionBarActivity  implements PersonalDetailFr
         } else {
             switchToPersonalDetails();
         }
+        mLocationClient = new LocationClient(this, this, this);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+//        mLocationClient.connect();
+    }
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+//        mLocationClient.disconnect();
+        super.onStop();
     }
 
     private boolean checkDetailsExist() {
@@ -80,5 +116,135 @@ public class MainActivity extends ActionBarActivity  implements PersonalDetailFr
         FragmentTransaction transaction = fm.beginTransaction();
         transaction.replace(R.id.contentFragment, fragment);
         transaction.commit();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        // Display the connection status
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onDisconnected() {
+        // Display the connection status
+        Toast.makeText(this, "Disconnected. Please re-connect.",
+            Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        this.connectionResult = connectionResult;
+                /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                    this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            //TODO showErrorDialog(connectionResult.getErrorCode());
+        }
+
+    }
+
+    // Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+
+    /*
+     * Handle results returned to the FragmentActivity
+     * by Google Play services
+     */
+    @Override
+    protected void onActivityResult(
+        int requestCode, int resultCode, Intent data) {
+        // Decide what to do based on the original request code
+        switch (requestCode) {
+        case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+            /*
+             * If the result code is Activity.RESULT_OK, try
+             * to connect again
+             */
+            switch (resultCode) {
+            case Activity.RESULT_OK :
+                    /*
+                     * Try the request again
+                     */
+                break;
+            }
+        }
+    }
+
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode =
+            GooglePlayServicesUtil.
+                isGooglePlayServicesAvailable(this);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates",
+                "Google Play services is available.");
+            // Continue
+            return true;
+            // Google Play services was not available for some reason
+        } else {
+            // Get the error code
+            int errorCode = connectionResult.getErrorCode();
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                errorCode,
+                this,
+                CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment =
+                    new ErrorDialogFragment();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(getSupportFragmentManager(),
+                    "Location Updates");
+            }
+        }
+        return false;
     }
 }
