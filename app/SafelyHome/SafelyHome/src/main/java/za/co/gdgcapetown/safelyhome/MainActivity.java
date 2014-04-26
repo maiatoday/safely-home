da984110c5769c808ba314f75848b3da9a344148
maia grotepass <maiatoday@gmail.com>
2014-04-26 12:51:20 +0200

    Make it switch fragments

--- app/SafelyHome/SafelyHome/src/main/java/za/co/gdgcapetown/safelyhome/MainActivity.java
+++ app/SafelyHome/SafelyHome/src/main/java/za/co/gdgcapetown/safelyhome/MainActivity.java
@@ -1,11 +1,15 @@
 package za.co.gdgcapetown.safelyhome;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import za.co.gdgcapetown.safelyhome.ui.IncidentFragment;
 import za.co.gdgcapetown.safelyhome.ui.PersonalDetailFragment;
 
 
@@ -17,6 +21,7 @@ public class MainActivity extends ActionBarActivity  implements PersonalDetailFr
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switchToPersonalDetails();
     }
 
 
@@ -31,18 +36,34 @@ public class MainActivity extends ActionBarActivity  implements PersonalDetailFr
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // as you specifyswitchToPersonalDetails a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            // TODO switch to the personal details fragment
+            switchToPersonalDetails();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void switchToPersonalDetails() {
+        Fragment fragment = PersonalDetailFragment.newInstance();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.contentFragment, fragment);
+        transaction.commit();
+    }
+
     @Override
     public void onDetailsOk() {
         Log.d(TAG, "The fragment pressed ok");
-        // TODO switch to the incident fragment
+        switchToIncidentReport();
+    }
+
+    private void switchToIncidentReport() {
+        Fragment fragment = IncidentFragment.newInstance();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.contentFragment, fragment);
+        transaction.commit();
     }
 }
